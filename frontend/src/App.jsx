a08341905366436fcc6e6c6934320a91bc059a4b
Mostak-Ahamed-Nishat <mostakahamed484@gmail.com>
2024-04-04 13:00:21 +0600

    navbar and home component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import "./App.css";
+import Header from "./components/Header";
+import { Outlet } from 'react-router-dom';
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <h1>Hello world!</h1>
+      <Header />
+      <Container className="my-2">
+        <Outlet/>
+      </Container>
     </>
   );
 }
